85f4ac2da5d806760b253cf76a04e08982f594f8
Alisa S <89223502125as@gmail.com>
2023-01-26 20:37:32 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 5000;
+        int mile1 = 20;
+        int bonusMile = ticketPrice / mile1;
+
+        System.out.println("Бонусных миль: " + bonusMile);
+
+    }
+}
